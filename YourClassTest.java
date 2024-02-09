f5d37835115b63713ef3b59d3fe9aa5782c5bf5d
Tarun <banditarunsai5566@gmail.com>
2024-02-09 19:57:35 +0530

    First commit

---
+++ YourClassTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class YourClassTest {
+    @Test
+    public void testHello() {
+        YourClass instance = new YourClass();
+        assertNotNull(instance);
+    }
+}
